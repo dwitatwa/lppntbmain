32c2743f26bd69a42227a10e396437b99918b063
dwitatwa <agungdwitatwa@gmail.com>
2021-05-21 09:22:30 +0800

    update redirect

--- next.config.js
+++ next.config.js
@@ -13,7 +13,13 @@ module.exports = {
       {
         source: "/tracerawardee2021/rusia",
         destination:
-          "https://docs.google.com/forms/d/e/1FAIpQLSf9t9rrykGFUFJcuNYP7x3kSTt9DFbK34Thh0oNKwzzQgu-tQ/viewform?usp=sf_link",
+          "https://docs.google.com/forms/d/e/1FAIpQLSfUhr6kOfkA5vqbWlCMXS4jQOBjUurTJnlJweF5E0TfIgSbvg/viewform?usp=sf_link",
+        permanent: true,
+      },
+      {
+        source: "pendaftaranbeasiswantb2021",
+        destination:
+          "https://docs.google.com/forms/d/e/1FAIpQLScgkM35L_CW8cNs3aLQQCo8oJxAfn9jElE0AudVYB8NukyAmw/viewform?usp=sf_link",
         permanent: true,
       },
     ];
