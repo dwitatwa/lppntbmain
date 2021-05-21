faf6c5f13cbfc29cdaefe12a0cfe6d5a9bb97b8a
dwitatwa <agungdwitatwa@gmail.com>
2021-05-21 09:19:07 +0800

    update redirect

--- next.config.js
+++ next.config.js
@@ -5,13 +5,15 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: "/cekredir",
-        destination: "https://google.com",
+        source: "/sosialisasibeasiswantb2021",
+        destination:
+          "https://docs.google.com/forms/d/e/1FAIpQLSf9t9rrykGFUFJcuNYP7x3kSTt9DFbK34Thh0oNKwzzQgu-tQ/viewform?usp=sf_link",
         permanent: true,
       },
       {
-        source: "/facebook",
-        destination: "https://facebook.com",
+        source: "/tracerawardee2021/rusia",
+        destination:
+          "https://docs.google.com/forms/d/e/1FAIpQLSf9t9rrykGFUFJcuNYP7x3kSTt9DFbK34Thh0oNKwzzQgu-tQ/viewform?usp=sf_link",
         permanent: true,
       },
     ];
