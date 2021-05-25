5b8e12dc3aa8dc8720f2724635793e80c2e38ac0
dwitatwa <agungdwitatwa@gmail.com>
2021-05-25 22:32:17 +0800

    update booklet

--- pages/perpustakaan/booklet.js
+++ pages/perpustakaan/booklet.js
@@ -34,14 +34,15 @@ const Booklet = ({ booklet19_20, booklet_guru_nakes, booklet_tahap2 }) => {
             <img src="/cobook.jpeg" alt="" />
             <div className={s.content}>
               <h1>Booklet Info</h1>
-              Tujuan Eropa 2021
+              Tujuan Eropa 2021 <br />
+              <span className={s.update_status}>(Update Negara Turki)</span>
               <div className={s.status}> Masih Berlaku</div>
               <Button
                 className={s.download}
                 size="small"
                 variant="outlined"
                 color="primary"
-                href="https://drive.google.com/file/d/1Tpfmi3D0jx8jAqeEMbJDAo-adNLgBsRq/view"
+                href="https://drive.google.com/file/d/1uUlA9gaWO8a4OnMXoyd8zdQEq8JHPJTg/view?usp=sharing"
                 target="_blank"
               >
                 Download
